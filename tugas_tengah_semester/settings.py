b6107b204ae58be684f5ab9fa3e2c8b947339d4f
danielcm585 <danielcm585@gmail.com>
2022-10-03 13:23:34 +0700

    Collect static

--- tugas_tengah_semester/settings.py
+++ tugas_tengah_semester/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
