19824d3653c6cd1e082426e49f75b857079d703e
danielcm585 <danielcm585@gmail.com>
2022-10-09 09:51:59 +0700

    Add app name

--- static/base.js
+++ static/base.js
@@ -1,14 +1,26 @@
-const drawer = document.getElementById("drawer")
+// const drawer = document.getElementById("drawer")
 
-const openMenu = () => {
-  drawer.classList.remove("right-[-100vw]")
-  drawer.classList.add("right-0")
-}
+// const openMenu = () => {
+//   drawer.classList.remove("right-[-100vw]")
+//   drawer.classList.add("right-0")
+// }
 
-const closeMenu = () => {
-  drawer.classList.remove("right-0")
-  drawer.classList.add("right-[-100vw]")
-}
+// const closeMenu = () => {
+//   drawer.classList.remove("right-0")
+//   drawer.classList.add("right-[-100vw]")
+// }
+
+$(document).ready(() => {
+  $('#drawer-open-button').click(() => {
+    $('#drawer').removeClass('right-[-100vw]')
+    $('#drawer').addClass('right-0')
+  })
+  
+  $('#drawer-close-button').click(() => {
+    $('#drawer').removeClass('right-0')
+    $('#drawer').addClass('right-[-100vw]')
+  })
+})
 
 // FIXME: Fix the message timeout
 // const message = document.getElementById("message")
