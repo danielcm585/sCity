79ecb8a006f2656c727a683d6cfdd2b9d0b22335
danielcm585 <danielcm585@gmail.com>
2022-10-12 22:06:05 +0700

    Update model

--- tender/models.py
+++ tender/models.py
@@ -15,7 +15,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     closed_at = models.DateTimeField()
-    chosen_registrant = models.OneToOneField('Registrant', on_delete=models.SET_NULL, null=True)
+    chosen_registrant_id = models.ForeignKey('Registrant', on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
 
