05e8894ae9e9867d462f99eede238c6aec9266ab
danielcm585 <danielcm585@gmail.com>
2022-10-27 22:24:39 +0700

    Fix some bugs

--- static/one_company.js
+++ static/one_company.js
@@ -11,6 +11,10 @@ $(document).ready(() => {
     company.projects.forEach((registrant) => {
       $('#projects-section').append(`
         <div onclick="location.href='/tender/project/${registrant.project.id}'" class="shadow-md rounded-lg flex-col justify-center hover:bg-gray-200 duration-300">
+<<<<<<< HEAD
+=======
+          <img src="${registrant.project.photo}" class="rounded-t-lg">
+>>>>>>> 0cbcf13 (Fix some bugs)
           <div class="p-4 w-full rounded-b-lg">
             <h1 class="font-bold text-xl text-center">
               ${registrant.project.title}
