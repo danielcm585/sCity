5634046b2c97a2b3882899c06cd8ecbae94d82ff
danielcm585 <danielcm585@gmail.com>
2022-10-31 10:52:20 +0700

    Add image model

--- tender/views/image_view.py
+++ tender/views/image_view.py
@@ -1,9 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from tender.forms.image_form import ImageForm
 from tender.models.image_model import Image
 from tender.serializers.image_serializer import ImageSerializer
 
+@api_view(['POST'])
 def new_image(request):
     def post():
         # Create new image
@@ -16,6 +18,7 @@ def new_image(request):
 
     if (request.method == 'POST'): return post()
 
+@api_view(['GET'])
 def one_image(request, id):
     def get():
         image = Image.objects.get(id=id)
--- tugas_tengah_semester/urls.py
+++ tugas_tengah_semester/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     path("authentication/", include("authentication.urls")),
     path("tender/", include("tender.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
