7c6550abb66ced52fbab21310cb9db3fb5b654ed
naufal.fikri11 <naufal.fikri11@ui.ac.id>
2022-11-02 20:12:12 +0700

    fixing bugs

--- marine/models.py
+++ marine/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Items(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     photo = models.ImageField(null=True, blank=True, upload_to="images/")  #Ganti foto
     photo_url = models.TextField(null=True)
     title = models.CharField(max_length=255)
