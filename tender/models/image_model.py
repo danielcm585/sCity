eb1ffcfa246f2f6a8e067cc631a412f003f13eef
danielcm585 <danielcm585@gmail.com>
2022-11-02 15:25:39 +0700

    Fix tender app

--- tender/models/image_model.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Image(models.Model):
-    id = models.AutoField(primary_key=True)
-    image = models.ImageField(upload_to='images/')
--- tender/serializers/project_serializer.py
+++ tender/serializers/project_serializer.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
 from tender.models.project_model import Project
-from tender.serializers.image_serializer import ImageSerializer
 from tender.serializers.registrant_serializer import RegistrantSerializer
 
 class ProjectSerializer(serializers.ModelSerializer):
     registrants = RegistrantSerializer(many=True, read_only=True)
-    image = ImageSerializer(read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id','title','image','description','closed_at','registrants','is_closed','created_at','edited_at','closed_at']
+        fields = ['id','title','description','closed_at','registrants','is_closed','created_at','edited_at','closed_at']
