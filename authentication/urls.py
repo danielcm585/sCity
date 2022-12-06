7f845b8dc62527f86ac18223a82c8f5aef434065
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2022-12-06 16:01:19 +0700

    Create auth api routes

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('register/', register_user, name='register'),
     path('logout/', logout_user, name='logout'),
+    path('api/login/', login_api, name='login_api'),
+    path('api/register/', register_api, name='register_api'),
 ]
