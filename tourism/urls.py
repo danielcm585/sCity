e8d9863d0a681f2e9b33c314334e73099b68ae83
jessicalambok14 <jessicalambok14@gmail.com>
2022-12-17 13:48:23 +0700

    update django

--- tourism/urls.py
+++ tourism/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', show_tourism, name='show_tourism'),
     path('add/', add_tourism, name='add_tourism'),
     path('add_visitor/<int:id>', add_visitor, name='add_visitor'),
+    path('delete/', delete_tourism, name='delete_tourism'),
+    path('add_visitor_flutter/<int:id>', add_visitor_flutter, name='add_visitor_flutter'),
     path('json', get_tourism_json, name='get_tourism_json'),
+    path('add_flutter/', add_tourism_flutter, name='add_tourism_flutter'),
+    path('adminCheck',admin_check_api,name='admin_check_api'),
 ]
--- tourism/views.py
+++ tourism/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import  HttpResponse, HttpResponseNotFound, JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -45,7 +46,7 @@ def add_tourism(request):
         return render(request, 'create.html', context)
     HttpResponseNotFound
 
-@login_required(login_url='/authentication/login/')
+# @login_required(login_url='/authentication/login/')
 def get_tourism_json(request):
     place = Place.objects.all()
     return HttpResponse(serializers.serialize('json', place), content_type='application/json')
@@ -59,4 +60,49 @@ def add_visitor(request, id):
         place.save()
         response['visitor'] = place.visitor
         return JsonResponse(response)
+        
+@csrf_exempt
+def add_visitor_flutter(request, id):
+    response = {}
+    if request.method == 'GET':
+        place = Place.objects.get(pk=id)
+        place.visitor = place.visitor + 1
+        place.save()
+        response['visitor'] = place.visitor
+        return JsonResponse({
+            'status': True,
+            'message': 'Visitor Berhasil ditambahkan!',
+        }, status=200)
+       
+@csrf_exempt
+def delete_tourism(request):
+     Place.objects.all().delete()
+     return JsonResponse({
+            'status': True,
+            'message': 'Place Berhasil dihapus!',
+        }, status=200)
+
+@csrf_exempt
+def add_tourism_flutter(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        price = request.POST.get("price")
+        image = request.POST.get("image")
+        place = Place(price=price, name=name,
+                                description=description, image=image)
+        place.save()
+        return JsonResponse({
+            'status': True,
+            'message': 'Data Berhasil ditambahkan!',
+        }, status=200)   
+
+def admin_check_api(request):
+    def get():
+        if request.user.is_authenticated:
+            if request.user.is_superuser:
+                return JsonResponse({'status': 201, 'message': 'Admin'}, status=201)
+            return JsonResponse({'status': 402, 'message': 'You are not admin'}, status=402)
+        return JsonResponse({'status': 401, 'message': 'You must be logged in'}, status=401)
+    if (request.method == 'GET'): return get()             
 
